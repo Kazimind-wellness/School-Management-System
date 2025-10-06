d789bab73c4604ccd7967b90933b4d23c965cd47
Steven Njoroge <stevenmacharia2003@gmail.com>
2025-10-06 11:02:41 +0300

    used new iproved blades file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,21 +8,28 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    {{-- ❌ Remove this line --}}
-    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
-
-    {{-- ✅ Use Vite --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <style>[x-cloak]{display:none !important;}</style>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <title>{{ $title }}</title>
+    <meta name="description" content="{{ $metaDescription }}">
+
+    {{-- ✅ Classic CSS files --}}
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/jquery.fancybox.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    {{-- Optional Theme Colors (if used) --}}
+    <link rel="stylesheet" href="{{ asset('css/theme-color/default-theme.css') }}">
+
+    {{-- Google Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
+        rel="stylesheet">
+
     @livewireStyles
 </head>
-
 
 <body class="custom-scrollbar">
     <div x-data="{ menuOpen: false }" class="min-h-screen flex">
@@ -39,14 +46,27 @@
         </div>
     </div>
 
+    {{-- ✅ Classic JS files --}}
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+    <script src="{{ asset('js/waypoints.js') }}"></script>
+    <script src="{{ asset('js/jquery.counterup.js') }}"></script>
+    <script src="{{ asset('js/jquery.mixitup.js') }}"></script>
+    <script src="{{ asset('js/jquery.fancybox.pack.js') }}"></script>
+    <script src="{{ asset('js/slick.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+
     {{-- Livewire Scripts --}}
     @livewireScripts
 
-    {{-- AlpineJS (if not bundled in app.js) --}}
+    {{-- AlpineJS --}}
     <script src="//unpkg.com/alpinejs" defer></script>
 
-    {{-- Livewire + Turbolinks Bridge --}}
-    <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js"
-            data-turbolinks-eval="false" data-turbo-eval="false"></script>
+    {{-- Turbolinks Bridge (optional) --}}
+    <script
+        src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js"
+        data-turbolinks-eval="false"
+        data-turbo-eval="false">
+    </script>
 </body>
 </html>
